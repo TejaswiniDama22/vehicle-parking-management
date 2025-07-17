2ec9020a1a13c7f18311c2c846249b8fd4b1ec76
TejaswiniDama22 <damateja167@gmail.com>
2025-07-17 20:44:44 +0530

    First commit

---
+++ src/main/java/cfg/lms/vms/VehicleParkingManagementApplication.java
@@ -0,0 +1,13 @@
+package cfg.lms.vms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleParkingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleParkingManagementApplication.class, args);
+	}
+
+}
