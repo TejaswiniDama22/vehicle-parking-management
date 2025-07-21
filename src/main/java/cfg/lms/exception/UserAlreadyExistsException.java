b0308f943ba2aeb00be7d264125654c006ba60dc
rajput-code <harshrajputrocks@gmail.com>
2025-07-21 17:03:59 +0530

    Deleted all project files

--- src/main/java/cfg/lms/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package cfg.lms.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
